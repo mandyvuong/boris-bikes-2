61afbb8028523f1af7ec6bbe3d63db4c03c32598
Daniel Clements <clemies2000@hotmail.com>
2021-08-24 16:39:06 +0100

    Complete step 8

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,7 +1,8 @@
 require "DockingStation.rb"
 
-describe "DockingStation" do
+describe DockingStation do
   it 'contains a DockingStation class' do
     expect dockingstation = DockingStation.new
   end
+  it {is_expected.to respond_to :release_bike }
 end
