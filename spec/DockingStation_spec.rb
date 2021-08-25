0e02677e276c841b5b8aad9db79226743b1fb82b
Amanda Vuong <mandyvuong@gmail.com>
2021-08-25 16:25:56 +0100

    step 14

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -19,26 +19,7 @@ describe DockingStation do
     end
   end
   it { is_expected.to respond_to(:dock).with(1).argument }
-  it { is_expected.to respond_to(:bike) }
 
-  it 'docks something' do
-    bike = Bike.new
-    # We want to return the bike we dock
-    expect(subject.dock(bike)).to eq bike
-  end
-
-  it 'returns docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    # Again, we need to return the bike we just docked
-    expect(subject.bike).to eq bike
-  end  
-  describe '#dock' do
-    it 'raises an error when full' do
-      subject.dock(Bike.new)
-      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
-    end
-  end
   describe '#dock' do
     it 'raises an error when full' do
       20.times { subject.dock Bike.new }
